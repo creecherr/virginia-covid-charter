301360202c1068d81e92cbba92722c701104dccd
Haley Creech <haley.creech@ticketmaster.com>
2020-05-07 14:40:11 -0400

    Made the service layer a class instead of functions. Also added additional error handling

--- covidcharter/__init__.py
+++ covidcharter/__init__.py
@@ -1,2 +1,2 @@
 from covidcharter.main import graph
-from covidcharter.service import filter_by_county, plot_data, get_all_counties
+from covidcharter.service import VirginiaData
--- covidcharter/main.py
+++ covidcharter/main.py
@@ -1,17 +1,21 @@
 import click
-from covidcharter.service import filter_by_county, plot_data, get_all_counties
+from covidcharter.service import VirginiaData
 
 
 @click.command()
 @click.option('--county', help='The county or locality to get the graph for')
 def graph(county):
+    service = VirginiaData()
     try:
         click.echo('Graphing the data...')
-        df = filter_by_county(county)
-        plot_data(df)
-    except:
-        counties = get_all_counties()
-        click.echo('Please try again. The covidcharter data chart generator requires a county from the following list: %s'
+        df = service.filter_by_county(county)
+        service.plot_data(df)
+    except SystemError:
+        click.echo("The Virginia Department of Health data is not available at this time, please try again later")
+    except ValueError:
+        counties = service.get_all_counties()
+        click.echo('Please try entering a different county or check the spelling. The covidcharter data chart generator '
+                   'requires a county from the following list: %s'
                    % counties)
 
 
